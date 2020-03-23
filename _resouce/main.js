dab2b15d73b4e570b10f14d84304cb87a08fc4ca
kylemclaren <kyle.mclaren1@gmail.com>
2020-03-24 01:00:09 +0200

    material UI button styling

--- _resouce/main.js
+++ _resouce/main.js
@@ -1,6 +1,11 @@
 // CSS
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./css/style.css";
+import "./styles/buttons.scss";
 
 // JS
 import "./js/script.js";
+
+// https://material.io/develop/web/components/buttons/floating-action-buttons/
+// import { MDCRipple } from "@material/ripple";
+// const fabRipple = new MDCRipple(document.querySelector(".mdc-fab"));
